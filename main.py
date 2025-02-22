5c9eb0c1217e79f384dfeef55bd1c0d077f4f33b
Mehmet Fatih Karaman <102466129+KaramanMehmet7@users.noreply.github.com>
2025-02-22 22:35:23 +0300

    Add files via upload
    İlk yükleme

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder
+import schedule
+import time
+import asyncio
+
+
+API_TOKEN = "7864569160:AAF0xn7iVgfsKComkBXx5Mwhyv6SZFleFU8"
+CHAT_ID = "5578759592"
+
+
+app = ApplicationBuilder().token(API_TOKEN).build()
+
+
+async def olum_hatirlat():
+    mesaj = "Fani'den faniye mesaj. Ölüm var!"
+    await app.bot.send_message(chat_id=CHAT_ID, text=mesaj)
+    print("Mesaj Gönderildi!")
+
+
+schedule.every().day.at("00:00").do(lambda: asyncio.run(olum_hatirlat()))
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)  
